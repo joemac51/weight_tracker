948d674da4bb69e8c678948be7ea3b6da9346286
Daniel Horrigan <daniel.horrigan2@mail.dcu.ie>
2020-02-25 15:07:59 +0000

    Overwrite weight when entered more than once on the same day

--- tracker.py
+++ tracker.py
@@ -25,29 +25,25 @@ class tracker:
 
     def log_weight(self, weight):
         self.weight = weight
-        # with open(self.JSON_PATH, 'r') as f:
-        #     self.data = json.load(f)
-        self.data.append({
-            "Date": date.today().strftime("%d/%m/%Y"),
-            "Weight": self.weight
-            })
+        if self.data[len(self.data)-1]["Date"] == date.today().strftime("%d/%m/%Y"):
+            self.data[len(self.data)-1]["Weight"] = self.weight
+        else:
+            self.data.append({
+                "Date": date.today().strftime("%d/%m/%Y"),
+                "Weight": self.weight
+                })
         with open(self.JSON_PATH, 'w') as f:
             json.dump(self.data, f)
 
     def plot_weight(self):
         with open(self.JSON_PATH, 'r') as f:
             contents = json.load(f)
-            # contents_list = contents.split()
-            # # remove units from weights and convert them to numbers
-            # contents_list_y = [float(i.rstrip('kg')) for i in contents_list if i.endswith('kg')]
-            # # remove semicolon from dates
-            # contents_list_x = [i.rstrip(':') for i in contents_list if str(i).endswith(':')]
             x_vals = [contents[i]["Date"] for i in range(len(contents))] # list of dates
             y_vals = [contents[i]["Weight"] for i in range(len(contents))] # list of weights
-            sns.lineplot(x_vals, y_vals, markers=True)
+            sns.lineplot(x_vals, y_vals, color='red', marker='o', mec='black', mfc='black')
             plt.show()
 
 if __name__=="__main__":
     weight_tracker = tracker()
-    weight_tracker.log_weight(77.5)
+    weight_tracker.log_weight(77.7)
     weight_tracker.plot_weight()
